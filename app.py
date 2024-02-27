058f4ac9b2eef177c66e83f3f37379eedc215745
Lou Gesite <133773207+louvreeee@users.noreply.github.com>
2024-02-27 23:49:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,10 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
 Session(app)
 
-firebase_creds_json = json.dumps({
+firebase_creds = {
     "type": os.getenv("FIREBASE_TYPE"),
     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    # Replace the escaped newline characters with actual newlines
     "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
@@ -33,7 +32,7 @@ firebase_creds_json = json.dumps({
     "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
     "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
     "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
-})
+}
 
 # Convert the JSON string back into a dictionary
 #firebase_creds = json.loads(firebase_creds_json)
